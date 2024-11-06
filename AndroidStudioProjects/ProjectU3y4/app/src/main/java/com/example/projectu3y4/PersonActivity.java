e44e69a279831328e70f3fb8bcef34cefa891baf
JairCamposG117 <jaircamposg117@gmail.com>
2024-11-06 08:05:41 -0600

    subir proyecto

--- AndroidStudioProjects/ProjectU3y4/app/src/main/java/com/example/projectu3y4/PersonActivity.java
+++ AndroidStudioProjects/ProjectU3y4/app/src/main/java/com/example/projectu3y4/PersonActivity.java
@@ -1,4 +1,48 @@
 package com.example.projectu3y4;
 
-public class PersonActivity {
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+public class PersonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_person);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_home);
+
+        bottomNavigationView.setOnItemReselectedListener(item -> {
+            if (item.getItemId()==R.id.bottom_home) {
+                startActivity(new Intent(getApplicationContext(), WelcomeActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+            }
+            if (item.getItemId()==R.id.bottom_search){
+                startActivity(new Intent(getApplicationContext(), SearchActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+            }
+            if (item.getItemId()==R.id.bottom_settings){
+                startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+            }
+            if (item.getItemId()==R.id.bottom_person);
+        });
+    }
 }
